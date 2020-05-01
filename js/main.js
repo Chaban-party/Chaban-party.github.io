67491e27d93d1e20f9e68122cc32d13e754084e2
Chaban-party <53243584+Chaban-party@users.noreply.github.com>
2020-05-01 22:22:21 +0300

    login
    был реализована станица входа

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
-$(function(){
-    //code
-})
+$(()=>{
+    // preloader
+    setInterval(() => {
+        let plr = $(".preloader");
+        plr.css('opacity', 0);
+        setTimeout(() => {
+            if (plr.css('opacity', 0))
+            plr.remove();
+        }, 300);
+    }, 1000);
+
+    // button home
+    let btn = $(".btn-start");
+    let form = $(".form");
+    setInterval(() => {
+        if (form.val() != ""){
+            btn.addClass("btn-start--active");          
+        }
+        else{
+            btn.removeClass("btn-start--active");
+        }
+    }, 100);
+    // при нажатии на кнопку или enter
+    btn.click(function(){
+        if (form.val()=="8520"){
+            btn.attr("href", "home.html");
+        }
+        else{
+            btn.attr("href", "#");
+            $(".massage").addClass("massage-active");
+        }
+    })
+    $("body").keypress(function(e) {
+        if (e.which == 13) {
+            if (form.val()=="8520"){
+                $(location).attr("href", "home.html");
+            }
+            else{
+                btn.attr("href", "#");
+                $(".massage").addClass("massage-active");
+            }
+        }
+   });
+});
