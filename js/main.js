5a0fd38891c9ff22b04e92cb401ea72d55eb9a60
Chaban-party <53243584+Chaban-party@users.noreply.github.com>
2020-05-02 18:36:48 +0300

    login 1.0
    добавленно:
    1) Двейн Джонсон
    2) Адаптив
    3) Анимация

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(()=>{
     // preloader
     setInterval(() => {
         let plr = $(".preloader");
+
         plr.css('opacity', 0);
         setTimeout(() => {
             if (plr.css('opacity', 0))
@@ -12,6 +13,7 @@ $(()=>{
     // button home
     let btn = $(".btn-start");
     let form = $(".form");
+
     setInterval(() => {
         if (form.val() != ""){
             btn.addClass("btn-start--active");          
@@ -20,25 +22,63 @@ $(()=>{
             btn.removeClass("btn-start--active");
         }
     }, 100);
+
     // при нажатии на кнопку или enter
-    btn.click(function(){
+    let n = 0;
+
+    function buttonClick(){
         if (form.val()=="8520"){
             btn.attr("href", "home.html");
         }
         else{
+            n++;
+            if (n == 3)
+                $(location).attr("href", "https://www.xvideos.com/tags/gay-porn");  
+            if (n == 2)
+                $(".massage").text("пароль опять не верный, уходи предупреждаю !!!");
             btn.attr("href", "#");
             $(".massage").addClass("massage-active");
         }
+    }
+
+    btn.click(function(){
+        buttonClick();
     })
+
     $("body").keypress(function(e) {
         if (e.which == 13) {
             if (form.val()=="8520"){
                 $(location).attr("href", "home.html");
             }
             else{
+                n++;
+                if (n == 3)
+                    $(location).attr("href", "https://www.xvideos.com/tags/gay-porn");
                 btn.attr("href", "#");
                 $(".massage").addClass("massage-active");
             }
         }
    });
+
+   //nice dick
+
+   let tuch = 0;
+
+   $(".nice-dick__img").click(function(){
+        tuch++;
+        switch (tuch) {
+            case 1:
+                $(".nice-message__text").text("О да еще!");
+                break;
+            case 2:
+                $(".nice-message__text").text("хочешь развлечся ?");
+                break;
+            case 3:
+                $(".nice-message__text").text("тронь меня еще");
+                break;
+            case 4:
+                $(location).attr("href", "https://www.xvideos.com/tags/gay-porn");
+        }
+   })
+
 });
